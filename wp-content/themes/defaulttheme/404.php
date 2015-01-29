bfda07dadadc0f66a2a42f70e5fad6d823234a2a
thomzzzzz <thomastijdink@gmail.com>
2015-01-29 13:04:09 +0100

    404 page

--- wp-content/themes/defaulttheme/404.php
+++ wp-content/themes/defaulttheme/404.php
@@ -1,4 +1,9 @@
 <?php get_header(); ?>
 
+<div class="wrapper">
+
+    <h1>404: Pagina niet gevonden</h1>
+    <p>De pagina die je zoekt, kan niet worden geopend.</p>
+</div>
 
 <?php get_footer(); ?>
