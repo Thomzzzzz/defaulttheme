db57c8d6935de955b7f6e36a4763c30c58819ee8
thomzzzzz <thomastijdink@gmail.com>
2015-02-26 15:04:35 +0100

    Added index.php

---
+++ wp-content/themes/defaulttheme/index.php
@@ -0,0 +1,14 @@
+<?php   //  Template name: Homepage
+get_header(); ?>
+<div class="wrapper">
+<?php
+    if(have_posts()){
+        while(have_posts()){
+            the_post();
+            echo "<h1>" . the_title() . "</h1>";
+            the_content();
+        }
+    }
+?>
+</div>
+<?php get_footer(); ?>
