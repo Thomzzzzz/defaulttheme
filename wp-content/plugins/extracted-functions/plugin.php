554fc29b68aadbe66d725e306beee564b0f1fc76
thomzzzzz <thomastijdink@gmail.com>
2016-08-11 16:39:28 +0200

    Added plugin files

---
+++ wp-content/plugins/extracted-functions/plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin name: Basisfunctionaliteiten toevoegen
+ * Plugin URI:  http://thomastijdink.com
+ * Description: Laat functions.php leeg en voegt de benodigde basisfunctionaliteiten toe via deze plugin
+ * Version: 0.1
+ * Author: Thom
+ * Author URI: http://thomastijdink.com
+ */
+
+//  Need cookies? Here!
+//setcookie('testCookie', 'jawel hoor, ik sta hier', time() + ( 60 * 60 * 24 * 365), '/' );
+setlocale(LC_ALL, 'nl_NL');
+date_default_timezone_set('Europe/Amsterdam');
